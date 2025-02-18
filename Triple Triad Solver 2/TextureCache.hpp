16f2d218b97f9ce96b92ad3d664a0c87ded589bc
Kat-tulpa <56337080+Kat-tulpa@users.noreply.github.com>
2025-02-18 12:37:04 -0800

    Added the ability to print stars on the cards

--- Triple Triad Solver 2/TextureCache.hpp
+++ Triple Triad Solver 2/TextureCache.hpp
@@ -9,20 +9,33 @@ namespace TextureCache {
     static constexpr float CARD_HEIGHT = 126.f * SCALE_FACTOR;
     static constexpr float CARD_HALF_WIDTH = CARD_WIDTH / 2.f;
     static constexpr float CARD_HALF_HEIGHT = CARD_HEIGHT / 2.f;
+    static constexpr float RARITY_WIDTH = 27.f * SCALE_FACTOR;
+    static constexpr float RARITY_HEIGHT = 26.f * SCALE_FACTOR;
 
     inline static std::vector<SDL_Texture*> cardTextures;
+    inline static std::vector<SDL_Texture*> rarityTextures;
     inline static std::vector<bool> textureFound;
     inline static ID COLORED_BACKING[PLAYER_COUNT + 1];
+
+    static void textureAddRarity(const std::string& filePath) {
+        SDL_Texture* newTexture = IMG_LoadTexture(RENDERER, filePath.c_str());
+        if (!newTexture) {
+            std::cout << "Error: Failed to load texture in TextureCache::textureAddRarity() " << SDL_GetError() << std::endl;
+            newTexture = IMG_LoadTexture(RENDERER, MISSING_TEXTURE_FILEPATH);
+        }
+
+        rarityTextures.push_back(newTexture);
+    }
 
     static ID textureAddCard(const std::string& filePath) {
         SDL_Texture* newTexture = IMG_LoadTexture(RENDERER, filePath.c_str());
         bool textureMissing = false;
         if (!newTexture) {
-            std::cout << "Error: Failed to load texture in TextureCache::textureAddCard()" << SDL_GetError() << std::endl;
+            std::cout << "Error: Failed to load texture in TextureCache::textureAddCard() " << SDL_GetError() << std::endl;
             newTexture = IMG_LoadTexture(RENDERER, MISSING_TEXTURE_FILEPATH);
             textureMissing = true;
             if (!newTexture)
-                std::cout << "Error: Also failed to load missing texture in TextureCache::textureAddCard()" << SDL_GetError() << std::endl;
+                std::cout << "Error: Also failed to load missing texture in TextureCache::textureAddCard() " << SDL_GetError() << std::endl;
         }
 
         const ID index = cardTextures.size();
@@ -59,13 +72,19 @@ namespace TextureCache {
     }
 
     static void init() {
-        COLORED_BACKING[PLAYER_RED] = textureAddCard("RED CARD.png");
-        COLORED_BACKING[PLAYER_BLUE] = textureAddCard("BLUE CARD.png");
-        COLORED_BACKING[PLAYER_NONE] = textureAddCard("EMPTY CARD.png");
+        textureAddRarity("rarity1.png");
+        textureAddRarity("rarity2.png");
+        textureAddRarity("rarity3.png");
+        textureAddRarity("rarity4.png");
+        textureAddRarity("rarity5.png");
+
         for (int i = 0; i < CardCollection::cardCount(); i++) {
             const ID id = textureAddCard(CardCollection::name(i) + ".png");
             if (id != i)
                 std::cout << "Error: TextureCache entry ID didn't match the Card ID. Name was: " << CardCollection::name(i) << std::endl;
         }
+        COLORED_BACKING[PLAYER_RED] = textureAddCard("RED CARD.png");
+        COLORED_BACKING[PLAYER_BLUE] = textureAddCard("BLUE CARD.png");
+        COLORED_BACKING[PLAYER_NONE] = textureAddCard("EMPTY CARD.png");
     }
 }
